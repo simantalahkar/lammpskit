b9b250de859fcec731295c6dc3182a18d5329951
simantalahkar <simantalahkar@hotmail.com>
2025-07-01 15:19:49 +0200

    created test for read structure

---
+++ tests/test_ecellmodel/test_data_readers.py
@@ -0,0 +1,44 @@
+import os
+import tempfile
+import numpy as np
+from lammpskit.ecellmodel.filament_layer_analysis import read_structure_info
+
+def test_read_structure_info_minimal():
+    # Create a minimal .lammpstrj file with 13 columns in the atom section
+    content = """\
+ITEM: TIMESTEP
+1200000
+ITEM: NUMBER OF ATOMS
+5
+ITEM: BOX BOUNDS pp pp pp
+0.0 50.0
+0.0 50.0
+0.0 50.0
+ITEM: ATOMS id type q x y z ix iy iz vx vy vz c_eng
+1 2 0.1 1.0 2.0 3.0 0 0 0 0 0 0 0
+2 1 -0.2 2.0 3.0 4.0 0 0 0 0 0 0 0
+3 2 0.3 3.0 4.0 5.0 0 0 0 0 0 0 0
+4 1 -0.4 4.0 5.0 6.0 0 0 0 0 0 0 0
+5 2 0.5 5.0 6.0 7.0 0 0 0 0 0 0 0
+"""
+    with tempfile.NamedTemporaryFile('w+', delete=False) as tmp:
+        tmp.write(content)
+        tmp.flush()
+        tmp_path = tmp.name
+
+    # The function expects to skip the first line, so we simulate that
+    timestep, total_atoms, xlo, xhi, ylo, yhi, zlo, zhi = read_structure_info(tmp_path)
+
+    assert timestep == 1200000
+    assert total_atoms == 5
+    assert np.isclose(xlo, 0.0)
+    assert np.isclose(xhi, 50.0)
+    assert np.isclose(ylo, 0.0)
+    assert np.isclose(yhi, 50.0)
+    assert np.isclose(zlo, 0.0)
+    assert np.isclose(zhi, 50.0)
+
+    os.remove(tmp_path)
+
+
+
